d16965a2fa10ba35e1e1bd5dc65c3ab1d46144b0
Radek <radek.boronski@gmail.com>
2025-11-10 11:29:03 +0100

    Changed run_monitoring.sh

--- run_monitoring.sh
+++ run_monitoring.sh
@@ -2,13 +2,12 @@
 LOG_FILE="session_$(date +%Y%m%d_%H%M%S).log"
 
 psql -a -f performance_test.sql \
-     "sslmode=verify-ca \
-      sslrootcert=/Users/radek/.postgresql/server-ca.pem \
-      sslcert=/Users/radek/.postgresql/client-cert.pem \
-      sslkey=/Users/radek/.postgresql/client-key.pem \
-      hostaddr=34.32.113.115 \
-      port=5432 \
+     "hostaddr=<host> \
+      port=<port> \
       user=postgres \
-      dbname=performance_test" 2>&1 | tee "$LOG_FILE"
+      dbname=<db>" 2>&1 | tee "$LOG_FILE"
 
 echo "Log saved to: $LOG_FILE"
+
+
+
