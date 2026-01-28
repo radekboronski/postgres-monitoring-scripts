845d3baf0887ca2085ac334e35d2686f19aa8d5c
Radek <radek.boronski@gmail.com>
2026-01-28 12:22:41 +0100

    Add run_monitoring.sh

--- run_monitoring.sh
+++ run_monitoring.sh
@@ -11,3 +11,18 @@ echo "Log saved to: $LOG_FILE"
 
 
 
+
+
+#!/bin/bash
+LOG_FILE="raporty/sunflower_session_$(date +%Y%m%d_%H%M%S).log"
+
+export PGPASSWORD="qYh9z2PdKXRQQ6CDAqfa"
+
+psql -a -f performance_test.sql \
+     "host=localhost \
+      port=5432 \
+      user=monitoring_user \
+      dbname=sunflower" 2>&1 | tee "$LOG_FILE"
+
+echo "Log saved to: $LOG_FILE"
+
